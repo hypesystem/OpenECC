28155cfb2c0ee0f2fd3074768ea766bda1670e08
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-03-11 12:34:51 +0100

    First commit

---
+++ OpenECC/FiniteFieldCurvePoint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenECC
+{
+    public class FiniteFieldCurvePoint : Point
+    {
+        private readonly int _x, _y;
+        private readonly ICurve _curve;
+
+        public FiniteFieldCurvePoint(int x, int y, ICurve curve)
+        {
+            _x = x;
+            _y = y;
+            _curve = curve;
+        }
+
+        public override int X { get { return _x; } }
+        public override int Y { get { return _y; } }
+
+        public override Point Add(Point q)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Point Multiply(Point q)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Point Exponentiate(int x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OpenECC/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenECC
+{
+    public abstract class Point
+    {
+        public abstract int X { get; }
+        public abstract int Y { get; }
+
+        public abstract Point Add(Point q);
+        public abstract Point Multiply(Point q);
+        public abstract Point Exponentiate(int x);
+
+        #region operators
+        public static Point operator +(Point p, Point q)
+        {
+            return p.Add(q);
+        }
+
+        public static Point operator *(Point p, Point q)
+        {
+            return p.Multiply(q);
+        }
+
+        public static Point operator ^(Point p, int x)
+        {
+            return p.Exponentiate(x);
+        }
+        #endregion
+    }
+}
