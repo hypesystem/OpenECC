0e7256210b0920220b5a4d974a1005997251d0dd
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-03-15 15:17:02 +0100

    Added Test project

---
+++ OpenECCTest/FiniteFieldCurvePointTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenECC;
+
+namespace OpenECCTest
+{
+    [TestClass]
+    public class FiniteFieldCurvePointTest
+    {
+        FiniteFieldCurvePoint p, r, q;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestNegationCircular()
+        {
+            var neg_p = -p;
+            Assert.AreEqual(p, -neg_p);
+        }
+    }
+}
