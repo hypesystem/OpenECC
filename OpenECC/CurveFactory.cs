beae79c03cb73c67563ab1566e442397ac099ad3
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-03-15 19:11:12 +0100

    Added CurveFactory

---
+++ OpenECC/CurveFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenECC
+{
+    public class CurveFactory
+    {
+        //private static ICurve _m511 = new FiniteFieldCurve(new BigInteger(
+
+        public static ICurve M511
+        {
+            get
+            {
+                return null;
+            }
+        }
+    }
+}
--- OpenECC/FiniteFieldCurve.cs
+++ OpenECC/FiniteFieldCurve.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace OpenECC
 {
     public class FiniteFieldCurve : ICurve
     {
+        private readonly BigInteger _a, _b, _prime;
+
+        public FiniteFieldCurve(BigInteger a, BigInteger b, BigInteger prime)
+        {
+            _a = a;
+            _b = b;
+            _prime = prime;
+        }
     }
 }
