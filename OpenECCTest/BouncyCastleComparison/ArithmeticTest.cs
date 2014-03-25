0d4a09f03be0f124284d19bd60e48ac280a85d10
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-03-25 08:06:11 +0100

    Added comparative test Compare to BouncyCastle maths

---
+++ OpenECCTest/BouncyCastleComparison/ArithmeticTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenECCTest.BouncyCastleComparison
+{
+    [TestClass]
+    public class ArithmeticTest
+    {
+        OpenECC.ICurve OpenECCSimpleCurve, OpenECCSecp256k1;
+        Org.BouncyCastle.Math.EC.ECCurve BCSimpleCurve, BCSecp256k1;
+
+        [TestInitialize]
+        public void SetUpSimpleCurve()
+        {
+            OpenECCSimpleCurve = new OpenECC.WeierstrassCurve(new System.Numerics.BigInteger(3), new System.Numerics.BigInteger(6), new System.Numerics.BigInteger(7919));
+            BCSimpleCurve = new Org.BouncyCastle.Math.EC.FpCurve(new Org.BouncyCastle.Math.BigInteger("3"), new Org.BouncyCastle.Math.BigInteger("6"), new Org.BouncyCastle.Math.BigInteger("7919"));
+        }
+        
+        /*[TestInitialize]
+        public void SetUpSecp256k1()
+        {
+            OpenECCSecp256k1 = OpenECC.CurveFactory.secp256k1;
+            BCSecp256k1 = new Org.BouncyCastle.Math.EC.FpCurve( ... );
+        }*/
+
+        [TestMethod]
+        public void TestSimpleCurveAddition()
+        {
+
+        }
+    }
+}
