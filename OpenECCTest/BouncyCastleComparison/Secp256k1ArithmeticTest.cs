4667fc4525671c47d5b0868115359462fe2f815d
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-04-07 01:22:16 +0200

    Completed secp256k1 comparison test suite It now contains same tests as the SimpeleCurveArithmeticTest suite

--- OpenECCTest/BouncyCastleComparison/Secp256k1ArithmeticTest.cs
+++ OpenECCTest/BouncyCastleComparison/Secp256k1ArithmeticTest.cs
@@ -6,7 +6,7 @@ namespace OpenECCTest.BouncyCastleComparison
     [TestClass]
     public class Secp256k1ArithmeticTest
     {
-        private OpenECC.ICurve OpenECCSecp256k1;
+        private OpenECC.WeierstrassCurve OpenECCSecp256k1;
         private OpenECC.Point OpenECCGenerator, OpenECCPoint1, OpenECCPoint2;
         private Org.BouncyCastle.Math.EC.ECCurve BCSecp256k1;
         private Org.BouncyCastle.Math.EC.ECPoint BCGenerator, BCPoint1, BCPoint2;
@@ -53,6 +53,44 @@ namespace OpenECCTest.BouncyCastleComparison
         {
             AssertPointsAreEqual(BCPoint1, OpenECCPoint1);
             AssertPointsAreEqual(BCPoint2, OpenECCPoint2);
+        }
+
+        [TestMethod]
+        public void TestSecp256k1AddInfinity()
+        {
+            //Document that BouncyCastle does the right thing.
+            var bc_point = BCPoint1.Add(BCSecp256k1.Infinity);
+            Assert.AreEqual(BCPoint1, bc_point);
+
+            var oecc_point = OpenECCPoint1 + OpenECCSecp256k1.Infinity;
+            Assert.AreEqual(OpenECCPoint1, oecc_point);
+        }
+
+        [TestMethod]
+        public void TestSecp256k1Addition()
+        {
+            var bc_point = BCPoint1.Add(BCPoint2);
+            var oecc_point = OpenECCPoint1 + OpenECCPoint2;
+
+            AssertPointsAreEqual(bc_point, oecc_point);
+        }
+
+        [TestMethod]
+        public void TestSecp256k1Doubling()
+        {
+            var bc_point = BCPoint1.Add(BCPoint1);
+            var oecc_point = OpenECCPoint1 + OpenECCPoint1;
+
+            AssertPointsAreEqual(bc_point, oecc_point);
+        }
+
+        [TestMethod]
+        public void TestSecp256k1Subtraction()
+        {
+            var bc_point = BCPoint1.Subtract(BCPoint2);
+            var oecc_point = OpenECCPoint1 - OpenECCPoint2;
+
+            AssertPointsAreEqual(bc_point, oecc_point);
         }
     }
 }
