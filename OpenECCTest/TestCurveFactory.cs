7efcaf16855f95b2f9e5646b94a93b37e30c91cd
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-04-22 18:30:50 +0200

    Added TestCurveFactory and a test curve

---
+++ OpenECCTest/TestCurveFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OpenECC;
+using System.Numerics;
+
+namespace OpenECCTest
+{
+    /// <summary>
+    /// Provides small and simple curves to test on.
+    /// </summary>
+    class TestCurveFactory
+    {
+        public WeierstrassCurve SimpleCurve1
+        {
+            get
+            {
+                return new WeierstrassCurve(
+                    new BigInteger(4),
+                    new BigInteger(20),
+                    new BigInteger(29)
+                );
+            }
+        }
+    }
+}
