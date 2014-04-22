dc3a19efb1b2b9b98aaf8170829fb5375eba32ff
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-04-22 15:42:12 +0200

    ElGamal decryption

--- OpenECCEncryption/ElGamalCiphertext.cs
+++ OpenECCEncryption/ElGamalCiphertext.cs
@@ -5,10 +5,31 @@ namespace OpenECC.Encryption
 {
     public class ElGamalCiphertext : Ciphertext
     {
+        private Point _c1, _c2;
+
         public ElGamalCiphertext(Point c1, Point c2) : base ("/")
         {
+            _c1 = c1;
+            _c2 = c2;
+
             throw new NotImplementedException();
             //Should use base properly!
+        }
+
+        public Point C1
+        {
+            get
+            {
+                return _c1;
+            }
+        }
+
+        public Point C2
+        {
+            get
+            {
+                return _c2;
+            }
         }
     }
 }
--- OpenECCEncryption/ElGamalEncryptor.cs
+++ OpenECCEncryption/ElGamalEncryptor.cs
@@ -9,11 +9,15 @@ namespace OpenECC.Encryption
     {
         ICurve _curve;
         BigInteger _n;
+        byte[] rng_bytes;
 
         public ElGamalEncryptor(ICurve curve, BigInteger n)
         {
             _curve = curve;
             _n = n;
+
+            //Byte array with enough room for a number the size of n
+            rng_bytes = _n.ToByteArray();
 
             //What is prime order (n)???
             throw new NotImplementedException();
@@ -23,7 +27,7 @@ namespace OpenECC.Encryption
         {
             var M = RepresentPlaintextAsPoint(m);
 
-            var k = SelectK(_n);
+            var k = SelectK();
 
             var c1 = _curve.Generator * k;
             var c2 = M + (Q.Point * k);
@@ -36,22 +40,31 @@ namespace OpenECC.Encryption
             throw new NotImplementedException();
         }
 
-        public BigInteger SelectK(BigInteger n)
+        public BigInteger SelectK()
         {
-            //Make a byte array with room enough for the limit
-            byte[] bytes = n.ToByteArray();
-
             //Generate secure random byte sequence
             var rng = new RNGCryptoServiceProvider();
-            rng.GetBytes(bytes);
+            rng.GetBytes(rng_bytes);
 
-            var rand = new BigInteger(bytes);
+            var rand = new BigInteger(rng_bytes);
 
             //Fit in range [1;n-1]
-            return (rand % (n - 1)) + 1;
+            return (rand % (_n - 1)) + 1;
         }
 
         public Plaintext Decrypt(PrivateKey d, Ciphertext c)
+        {
+            if (!(c is ElGamalCiphertext))
+                throw new ArgumentException("Ciphertext must be ElGamalCiphertext!");
+
+            var ciphertext = c as ElGamalCiphertext;
+
+            var M = ciphertext.C2 - (ciphertext.C1 * d.Value);
+
+            return RepresentPointAsPlaintext(M);
+        }
+
+        private Plaintext RepresentPointAsPlaintext(Point p)
         {
             throw new NotImplementedException();
         }
