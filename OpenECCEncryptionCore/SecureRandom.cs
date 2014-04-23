b3dfb23b1d1cbc0bc08e427ad00db194c4d07b56
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-04-23 13:04:48 +0200

    Added SecureRandom number generator Generates BigIntegers "securely"

---
+++ OpenECCEncryptionCore/SecureRandom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Numerics;
+
+namespace OpenECC.Encryption.Core
+{
+    public class SecureRandom
+    {
+        private RNGCryptoServiceProvider _rng;
+
+        public SecureRandom()
+        {
+            _rng = new RNGCryptoServiceProvider();
+        }
+
+        /// <summary>
+        /// Equivalent of "return random x in [min;max]"
+        /// </summary>
+        public BigInteger GetBigInteger(BigInteger min, BigInteger max)
+        {
+            byte[] bytes = max.ToByteArray();
+            _rng.GetBytes(bytes);
+            var rand = new BigInteger(bytes);
+
+            return (rand % (max)) + min;
+        }
+
+        public BigInteger GetBigInteger(BigInteger max)
+        {
+            return GetBigInteger(BigInteger.Zero, max);
+        }
+    }
+}
