2d50dfda5e25a462dee6d6d16d8688b123af84e4
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-04-25 15:04:36 +0200

    Added message encoding

---
+++ OpenECCEncryption/IMessageEncoder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using OpenECC;
+using OpenECC.Encryption.Core;
+
+namespace OpenECC.Encryption
+{
+    public interface IMessageEncoder
+    {
+        Point EncodeMessage(Plaintext messageText);
+        Plaintext DecodeMessage(Point messagePoint);
+    }
+}
---
+++ OpenECCEncryption/ProbablisticWeierstrassMessageEncoder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+using OpenECC.Encryption.Core;
+
+namespace OpenECC.Encryption
+{
+    public class ProbablisticWeierstrassMessageEncoder : IMessageEncoder
+    {
+        private WeierstrassCurve _curve;
+        private BigInteger _k;
+
+        public ProbablisticWeierstrassMessageEncoder(WeierstrassCurve curve, BigInteger k)
+        {
+            _curve = curve;
+            _k = k;
+        }
+
+        public Point EncodeMessage(Plaintext message)
+        {
+            var m = message.ToBigInteger();
+
+            throw new NotImplementedException();
+        }
+
+        public Plaintext DecodeMessage(Point messagePoint)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
