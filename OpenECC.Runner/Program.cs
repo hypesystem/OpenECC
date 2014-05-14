f7a501d1f6fd3d2e7899cfa3f0f5dce46dd94ce1
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-05-14 15:28:56 +0200

    cleaning

--- OpenECC.Runner/Program.cs
+++ OpenECC.Runner/Program.cs
@@ -16,26 +16,14 @@ namespace OpenECC.Runner
         {
             var str = args[0];
 
-            Console.WriteLine("About to encrypt " + str);
-            Console.ReadKey();
-
             var curve = CurveFactory.secp256k1;
             var encoder = new ProbabilisticWeierstrassMessageEncoder(curve, new BigInteger(7));
             var encryptor = new ElGamalEncryptor(curve, encoder);
-
             var keys = encryptor.GenerateKeyPair();
-
             var plaintext = new Plaintext(str);
 
             var ciphertext = encryptor.Encrypt(keys.PublicKey, plaintext);
-
-            Console.WriteLine("Encrypted as " + ciphertext);
-            Console.ReadKey();
-
             var plaintext2 = encryptor.Decrypt(keys.PrivateKey, ciphertext);
-
-            Console.WriteLine("Decrypted as " + plaintext2);
-            Console.ReadKey();
         }
     }
 }
